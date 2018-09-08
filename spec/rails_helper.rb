499991cc70e5b65b3ddacbc8d7dfee70ddf7de1c
Walter A. Alves <walter.arruda.alves@gmail.com>
2018-09-08 04:29:41 -0300

    conf(tests): Add shoulda in project
    Add shoulda in Gemfile and in the rails_helper;
    Fix rails_helper;

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'database_cleaner'
+require 'shoulda/matchers'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
@@ -57,16 +58,23 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   
   # add FactoryBoy methods
-  config.include FactoryBot::Syntax::MEthods
+  config.include FactoryBot::Syntax::Methods
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
     DatabaseCleaner.strategy = :transaction
   end
 
-  Config.around(:each) do |example|
+  config.around(:each) do |example|
     DatabaseCleaner.cleaning do
       example.run
     end
   end
 
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
