bd2e8f1dd2372bbc5891d2deb0fa3da0f27b537e
Walter A. Alves <walter.arruda.alves@gmail.com>
2018-09-17 04:16:11 -0300

    Fix(models): Fix Document and Contact infos type column
    The column type in Document and Contact Infos was causing erros as
    single table inheritance column. The column was changed to document_type
    and contact_type respectively

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_15_014014) do
+ActiveRecord::Schema.define(version: 2018_09_17_071204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2018_09_15_014014) do
   end
 
   create_table "contact_infos", force: :cascade do |t|
-    t.string "type"
+    t.string "contact_type"
     t.string "info"
     t.bigint "owner_id"
     t.datetime "created_at", null: false
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2018_09_15_014014) do
 
   create_table "documents", force: :cascade do |t|
     t.string "number"
-    t.string "type"
+    t.string "document_type"
     t.bigint "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
