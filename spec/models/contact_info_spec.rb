a562385f3ab657510145675d2e10802c06b925c8
Walter A. Alves <walter.arruda.alves@gmail.com>
2018-09-17 04:28:12 -0300

    fix(tests): update tests and factories

--- spec/models/contact_info_spec.rb
+++ spec/models/contact_info_spec.rb
@@ -5,6 +5,6 @@ require 'rails_helper'
 RSpec.describe ContactInfo, type: :model do
   it { should belong_to(:owner) }
 
-  it { should validate_presence_of(:type) }
+  it { should validate_presence_of(:contact_type) }
   it { should validate_presence_of(:info) }
 end
